3083c7a127ec66673b9c7b2ea870f6c151016737
asma <benramoulasma@gmail.com>
2023-12-01 23:14:40 +0100

    hotreload_home

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: const Center(
+        child: Text('This is the home page',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+            )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.green,
+        child: const Text('Click'),
+      ),
+    );
+  }
+}
